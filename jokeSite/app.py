af0d0e81c650a668302a52f4b903b9b43c8bfb81
pilmeha <garmayev.ayur@inbox.ru>
2025-07-18 08:48:58 +0800

    норм версия сайта

--- jokeSite/app.py
+++ jokeSite/app.py
@@ -39,12 +39,24 @@ async def generate(request: Request, context: str = Form(...)):
             no_repeat_ngram_size=2,
             early_stopping=True,
             pad_token_id=tokenizer.pad_token_id,
-            eos_token_id = tokenizer.eos_token_id,
+            # eos_token_id = tokenizer.eos_token_id,
             do_sample=True,
             top_k=50,
             top_p=0.9,
             temperature=0.7,
+            num_return_sequences=1,
         )
-    joke = tokenizer.decode(output[0], skip_special_tokens=True).split("<|joke|>")[-1].strip()
+
+    decoded = tokenizer.decode(output[0], skip_special_tokens=False)
+    # Оставим только то, что идет после <|joke|>
+    if "<|joke|>" in decoded:
+        joke = decoded.split("<|joke|>")[-1]
+    else:
+        joke = decoded  # fallback
+    # Удалим возможные спецтокены в конце
+    for token in tokenizer.all_special_tokens:
+        joke = joke.replace(token, "")                          
+    joke = joke.strip()
+
 
     return templates.TemplateResponse("index.html", {"request": request, "joke": joke, "context": context})
